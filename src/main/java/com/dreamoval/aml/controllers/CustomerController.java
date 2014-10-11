91d8e9960878ff5873489916d65aaf95871891c0
dreamadmin <dreamadmin@do-enr-pc-8>
2014-10-11 06:33:11 +0000

    Added customer controller

--- src/main/java/com/dreamoval/aml/controllers/CustomerController.java
+++ src/main/java/com/dreamoval/aml/controllers/CustomerController.java
@@ -4,8 +4,12 @@
  */
 package com.dreamoval.aml.controllers;
 
+import com.dreamoval.aml.model.nodes.Customer;
+import com.dreamoval.aml.neo4j.NeoRestClient;
 import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -14,9 +18,10 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class CustomerController {
-    @RequestMapping("/customer/create")
-    public boolean createCustomer(HttpServletRequest request){
-        
-        return true;
+    @Autowired
+    NeoRestClient neo;
+    @RequestMapping(value="/customer/create", method=RequestMethod.POST, consumes="application/json")
+    public boolean createCustomer(Customer customer){
+        return neo.addCustomer(customer);
     }
 }
